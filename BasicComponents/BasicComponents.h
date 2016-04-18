5c676504a6b2dae6d1eaed2481dee0c1280d7184
Lasse Løvdahl <lasse.loevdahl@gmail.com>
2016-04-18 09:50:15 +0200

    Initial Commit

---
+++ BasicComponents/BasicComponents.h
@@ -0,0 +1,19 @@
+//
+//  BasicComponents.h
+//  BasicComponents
+//
+//  Created by Lasse Løvdahl on 18/04/2016.
+//  Copyright © 2016 Miracle A/S. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BasicComponents.
+FOUNDATION_EXPORT double BasicComponentsVersionNumber;
+
+//! Project version string for BasicComponents.
+FOUNDATION_EXPORT const unsigned char BasicComponentsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BasicComponents/PublicHeader.h>
+
+
