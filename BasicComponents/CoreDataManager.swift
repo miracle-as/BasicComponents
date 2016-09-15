9a18d729728c7829841a25e27f932dc3be5e2cb2
Lasse Løvdahl <lasse.loevdahl@gmail.com>
2016-09-15 12:53:38 +0200

    Swift 2.3

--- BasicComponents/CoreDataManager.swift
+++ BasicComponents/CoreDataManager.swift
@@ -65,7 +65,7 @@ public class CoreDataManager {
 			print("CoreData ERROR, There was an error creating or loading the application's saved data.")
 
 			do {
-				try NSFileManager.defaultManager().removeItemAtURL(url)
+				try NSFileManager.defaultManager().removeItemAtURL(url!)
 				try coordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: storeOptions)
 			} catch {
 				fatalError("CoreData ERROR, There was an error creating or loading the application's saved data.")
@@ -255,7 +255,7 @@ public extension NSManagedObject {
 			let fetchRequest = NSFetchRequest(entityName: entityName)
 			fetchRequest.predicate = predicate
 
-			return inContext.countForFetchRequest(fetchRequest, error: nil)
+			return (try? inContext.countForFetchRequest(fetchRequest)) ?? -1
 	}
 
 
