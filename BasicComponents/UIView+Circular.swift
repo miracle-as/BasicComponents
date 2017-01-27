bf81ca1cfb12f4bd76b80fda296a75469d469358
Lasse Løvdahl <lasse.loevdahl@gmail.com>
2017-01-27 09:42:41 +0100

    Vertical rounding

--- BasicComponents/UIView+Circular.swift
+++ BasicComponents/UIView+Circular.swift
@@ -9,15 +9,15 @@
 import UIKit
 
 public extension UIView {
-	@IBInspectable public var cornerRadius: CGFloat {
-		get {
-			return layer.cornerRadius
-		}
-		set {
-			layer.cornerRadius = newValue
-			layer.masksToBounds = newValue > 0
-		}
-	}
+  @IBInspectable public var cornerRadius: CGFloat {
+    get {
+      return layer.cornerRadius
+    }
+    set {
+      layer.cornerRadius = newValue
+      layer.masksToBounds = newValue > 0
+    }
+  }
   @IBInspectable public var border: CGFloat {
     get {
       return layer.borderWidth
@@ -35,6 +35,41 @@ public extension UIView {
     }
     set {
       layer.borderColor = newValue != .none ? newValue!.cgColor : .none
+    }
+  }
+
+  public var contentView: UIView? {
+    get {
+      return subviews.first
+    }
+
+    set {
+      guard
+        let view = newValue,
+        !subviews.contains(view) else {
+          return
+      }
+
+      let viewsToRemove = subviews
+
+      view.translatesAutoresizingMaskIntoConstraints = false
+      addSubview(view)
+
+      NSLayoutConstraint.activate([
+        view.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0),
+        view.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 0),
+        view.topAnchor.constraint(equalTo: topAnchor, constant: 0),
+        view.bottomAnchor.constraint(equalTo: bottomAnchor, constant: 0),
+        ])
+
+      if let fromView = viewsToRemove.first {
+        UIView.transition(from: fromView,
+                          to: view,
+                          duration: 0.5,
+                          options: .transitionCrossDissolve) { done in
+                            viewsToRemove.forEach { $0.removeFromSuperview() }
+        }
+      }
     }
   }
 }
